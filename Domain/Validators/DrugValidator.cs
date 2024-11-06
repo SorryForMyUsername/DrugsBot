0531d61ad1b7c01fcb507f32807e12279d3feb0a
Ilya Shichkin <ilyashichkin29@gmail.com>
2024-11-07 00:19:07 +0200

    Создание проекта

---
+++ Domain/Validators/DrugValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Domain.Entities;
+
+namespace Domain.Validators;
+
+public class DrugValidator : AbstractValidator<Drug>
+{
+    public DrugValidator()
+    {
+        RuleFor(d => d.Name)
+            .NotNull().WithMessage(ValidationMessage.NotNull)
+            .NotEmpty().WithMessage(ValidationMessage.NotEmpty)
+            .Length(2, 28).WithMessage(ValidationMessage.WrongLength);
+        RuleFor(d => d.CountryCodeId)
+            .Matches("");
+
+
+    }
+}
---
+++ Domain/Validators/ValidationMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Validators;
+
+public class ValidationMessage
+{
+    public static string NotNull = "{PropertyName} не может быть NULL";
+    public static string NotEmpty = "{PropertyName} не может быть пустым";
+    public static string WrongLength = "{PropertyName} должен быть от {min} до {max}";
+}
